e5e25b5a48ab7c3f545344bca9282c707aece8ed
FAREAST\suxi <suxi@microsoft.com>
2023-01-10 09:50:51 +0800

    add verify_conda.bat

---
+++ verify_conda.bat
@@ -0,0 +1,18 @@
+set environment=%1
+set python=%2
+call conda create -n %environment% python=%python% -y
+call conda activate %environment%
+call pip install -r src/requirements.txt
+
+if exist privatepkgs\ (
+	for %%f in (.\privatepkgs\*.whl) do (
+		echo %%f
+		call pip install %%f
+	)
+) else (
+	echo "No private packages"
+)
+
+pip install azureml-defaults
+pip install PyYAML
+pip install pyraisdk
