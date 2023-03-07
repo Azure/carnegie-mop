827185e8e241d52b0f4f60eb5137a9d0c9e523f6
FAREAST\suxi <suxi@microsoft.com>
2023-03-07 11:53:29 +0800

    update

--- packages/mop_utils/setup.py
+++ packages/mop_utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mop_utils',
-    version='1.5.2',
+    version='1.5.3',
     description='Utilities for MOP',
     packages=['mop_utils'],
     package_dir={'mop_utils': './mop_utils'},
