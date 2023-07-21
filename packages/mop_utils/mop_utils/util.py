dda6cbb05c2311679b6687f70c077c9698583b30
XiaoningJin <v-xiaonjin@microsoft.com>
2023-07-21 19:03:39 +0800

    1. add ACS image and text input type, 2, add ACS response type. 3. Change desciption

--- packages/mop_utils/mop_utils/util.py
+++ packages/mop_utils/mop_utils/util.py
@@ -5,46 +5,66 @@ Mop-utils some util.
 from typing import Dict, Union
 
 
+class ImageAnalysisInput:
+    data: bytes = b''
+
+
+class TextAnalysisInput:
+    text: str = ""
+
+
+class AnalysisResult:
+    harmful_score: float = 0.0  # confidence score
+    severity_level: int = 0     # predicted label
+
+
+class AcsResponse:
+    hate = AnalysisResult()
+    self_harm = AnalysisResult()
+    sexual = AnalysisResult()
+    violence = AnalysisResult()
+
+
 class PredictedLabel:
     """Predict label:
     for example:
-      "label_taxonomy": {
-            "label_name-1": 1,
-            "label_name-2": 0,
-            "label_name-3": 0
+      "label_name": {
+            "label_value_name-1": 1,
+            "label_value_name-2": 0,
+            "label_value_name-3": 0
       }
     """
     
-    def __init__(self, label: str, label_values: Dict[str, int]):
+    def __init__(self, label_name: str, label_values: Dict[str, int]):
         """
         Initialize label and label values
-        @param label: label (taxonomy) name.
-        @type label: str
+        @param label_name: label name.
+        @type label_name: str
         @param label_values: label values
-        @type label_values: Dict which key type is str, value type is int.
+        @type label_values: Dictionary which key type is str, value type is int, its 'value' should be 1 or 0.
         """
-        self.label = label
+        self.label_name = label_name
         self.label_values = label_values
-        self.validate()
+        # self.validate()
     
     def validate(self):
         """
-        validate taxonomy name, type and label name, type and value type.
+        Validates label name, type and label value name, type and value type.
         @return:
         @rtype:
         """
-        if not self.label or not isinstance(self.label, str):
-            raise TypeError(f"Label name must be type str and value must be not empty. {self.label}")
+        if not self.label_name or not isinstance(self.label_name, str):
+            raise TypeError(f"Label name must be type str and value must be not empty. {self.label_name}")
         
         if not self.label_values or not isinstance(self.label_values, dict):
             raise TypeError(f"Label scores must be dict. {self.label_values}")
         
         for k, v in self.label_values.items():
             if not k or not isinstance(k, str):
-                raise TypeError(f"Key of label value should be str and not empty: {k}")
+                raise TypeError(f"Key of label value should be type str and not empty: {k}")
             
             if v is None or not isinstance(v, int):
-                raise TypeError(f"Value of label value should be int: {k}, {v}, {type(v)}")
+                raise TypeError(f"The value of label value should be int: {k}, {v}, {type(v)}")
         
         number = len(self.label_values.keys())
         if 0 == number:
@@ -54,33 +74,34 @@ class PredictedLabel:
 class ConfidenceScore:
     """Confidence score:
     for example:
-        "label_taxonomy": {
-            "label_name-1": 0.3,
-            "label_name-2": 0,
-            "label_name-3": 0.2
+        "label_name": {
+            "label_value_name-1": 0.3,
+            "label_value_name-2": 0,
+            "label_value_name-3": 0.2
         }
     """
     
     def __init__(self, label_name: str, confidence_score: Dict[str, Union[float, int]]):
         """
         Initial confidence score.
-        @param label_name: label (taxonomy) name
+        @param label_name: label name
         @type label_name: str
         @param confidence_score: confidence score
-        @type confidence_score: Dict which key type is str, and value type is a float or int.
+        @type confidence_score: Dictionary which key type is str, and value type is a float or int,
+                                its 'value' should between 0 and 1.
         """
-        self.label: str = label_name
+        self.label_name: str = label_name
         self.scores: Dict[str, Union[float, int]] = confidence_score
-        self.validate()
+        # self.validate()
     
     def validate(self):
         """
-        validate taxonomy name, type and label name, type and value type.
+        Validates label name, type and label value name, type and value type.
         @return:
         @rtype:
         """
-        if not self.label or not isinstance(self.label, str):
-            raise TypeError(f"Label name must be str and not empty: {self.label}")
+        if not self.label_name or not isinstance(self.label_name, str):
+            raise TypeError(f"Label name must be str and not empty: {self.label_name}")
         
         if not self.scores or not isinstance(self.scores, dict):
             raise TypeError(f"Confidence scores must be dict and may not empty: {self.scores}")
