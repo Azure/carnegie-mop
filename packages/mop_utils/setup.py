6ec82dc5c3ae2f00172fc804817ce4e560d82522
XiaoningJin <v-xiaonjin@microsoft.com>
2023-07-28 17:04:28 +0800

    change setup for whl

--- packages/mop_utils/setup.py
+++ packages/mop_utils/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mop_utils',
-    version='1.8',
+    version='1.9rc1',
     description='Utilities for MOP',
     packages=['mop_utils'],
     package_dir={'mop_utils': './mop_utils'},
