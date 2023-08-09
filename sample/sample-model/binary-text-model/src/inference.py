d357d9d7742d8da7979ca8367c81dd1e981ce56c
XiaoningJin <v-xiaonjin@microsoft.com>
2023-08-09 17:26:40 +0800

    refine example of invoke order

--- sample/sample-model/binary-text-model/src/inference.py
+++ sample/sample-model/binary-text-model/src/inference.py
@@ -1,11 +1,9 @@
-import json
-import base64
 import pickle
 from typing import List, Dict
 
 import nltk
 from mop_utils.base_model_wrapper import BaseModelWrapper, MopInferenceInput, MopInferenceOutput
-from mop_utils.util import TextAnalysisInput, ImageAnalysisInput, AcsTextResponse, AcsImageResponse, AnalysisResult
+from mop_utils.util import TextAnalysisInput, AcsTextResponse, AnalysisResult
 
 
 class ModelWrapper(BaseModelWrapper):
@@ -22,6 +20,13 @@ class ModelWrapper(BaseModelWrapper):
         self.tokenizer = pickle.load(open(model_root + '/tokenizer.pkl', 'rb'))
 
     def inference(self, item: Dict) -> Dict:
+        """
+        Model Inference
+        @param item: A dictionary which have a key named 'data'.
+        @type item: Dictionary
+        @return: Inference score.
+        @rtype: Dictionary
+        """
         features = self.tokenizer.transform([item.get('data')])
         score = self.model.predict_proba(features)[0][1]
         return {'score': score}
@@ -95,16 +100,16 @@ class ModelWrapper(BaseModelWrapper):
 if __name__ == "__main__":
     model_wrapper = ModelWrapper()
     model_wrapper.init(model_root='../model')
-    customized_input = {"data": "NIGGER PLEASE \n EAT A COCK, LOL HY."}
-    c_output = model_wrapper.inference(customized_input)
-    print(c_output)
 
-    mop_input = MopInferenceInput(text="NIGGER PLEASE \n EAT A COCK, LOL HY.")
-    customized_input = model_wrapper.convert_mop_input_to_model_input(mop_input)
-    print(customized_input)
-    assert mop_input.text == customized_input.get('data')
+    mop_util_input = MopInferenceInput(text="NIGGER PLEASE \n EAT A COCK, LOL HY.")
+    model_input = model_wrapper.convert_mop_input_to_model_input(mop_util_input)
+    print(model_input)
+    assert mop_util_input.text == model_input.get('data')
 
-    mop_output = model_wrapper.convert_model_output_to_mop_output(c_output)
+    model_output = model_wrapper.inference(model_input)
+    print(model_output)
+    
+    mop_output = model_wrapper.convert_model_output_to_mop_output(model_output)
     print(mop_output)
     
     acs_txt_req = TextAnalysisInput()
@@ -112,64 +117,9 @@ if __name__ == "__main__":
     model_input = model_wrapper.convert_acs_text_request_to_model_inference_input(acs_txt_req)
     print(model_input)
     
-    model_inference_output = {
-        "violence": {
-            "scores": [
-                0.9343945384025574,
-                0.016118334606289865,
-                0.024537162855267526,
-                0.024950077757239343
-            ],
-            "class": 0
-        },
-        "self-harm": {
-            "scores": [
-                0.9265221357345581,
-                0.02502666786313057,
-                0.02527506835758686,
-                0.02317621186375618
-            ],
-            "class": 0
-        },
-        "hate": {
-            "scores": [
-                0.1400328278541565,
-                0.02049916982650757,
-                0.024420715868473054,
-                0.8150473237037659
-            ],
-            "class": 6
-        },
-        "sexual": {
-            "scores": [
-                0.882838249206543,
-                0.02585785463452339,
-                0.02786555513739586,
-                0.06343831121921539
-            ],
-            "class": 0
-        }
-    }
-    
-    acs_text_response = model_wrapper.convert_model_inference_output_to_acs_text_response(model_inference_output)
+    acs_text_response = model_wrapper.convert_model_inference_output_to_acs_text_response(model_output)
     print(acs_text_response.hate.harmful_score, acs_text_response.hate.severity_level,
           acs_text_response.sexual.harmful_score, acs_text_response.sexual.severity_level,
           acs_text_response.self_harm.harmful_score, acs_text_response.self_harm.severity_level,
           acs_text_response.violence.harmful_score, acs_text_response.violence.severity_level)
-
-    acs_image_req = ImageAnalysisInput()
-    image_data = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVR4nGP4//8/AAX+Av4N70a4AAAAAElFTkSuQmCC'
-    decoded_data = base64.b64decode(image_data)
-
-    print(decoded_data)
-    acs_image_req.data = decoded_data
-    model_input = model_wrapper.convert_acs_image_request_to_model_inference_input(acs_image_req)
-    assert image_data == model_input["data"]
-    
-    acs_image_response = model_wrapper.convert_model_inference_output_to_acs_image_response(model_inference_output)
-    print(acs_image_response.hate.harmful_score, acs_image_response.hate.severity_level,
-          acs_image_response.sexual.harmful_score, acs_image_response.sexual.severity_level,
-          acs_image_response.self_harm.harmful_score, acs_image_response.self_harm.severity_level,
-          acs_image_response.violence.harmful_score, acs_image_response.violence.severity_level)
-    
     
