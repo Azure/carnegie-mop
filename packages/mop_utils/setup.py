d4aa802049978522f8b0232fa94352481aefd599
Anwar Saipulla <asaipulla@microsoft.com>
2024-06-17 17:51:18 -0700

    change to free pinning in MOP lib so that each model can choose to pin a specific version of the pyraisdk

--- packages/mop_utils/setup.py
+++ packages/mop_utils/setup.py
@@ -8,7 +8,7 @@ setup(
     package_dir={'mop_utils': './mop_utils'},
     python_requires='>=3.8.0',
     install_requires=[
-        'pyraisdk ~= 0.2',
+        'pyraisdk >= 0.2',
         'PyYAML >= 6.0',
         'numpy >= 1.22.0',
     ]
