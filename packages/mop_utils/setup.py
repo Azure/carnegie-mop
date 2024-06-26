158b35a25f28aeb62b237e87c1bf2350abfda942
Anwar Saipulla <asaipulla@microsoft.com>
2024-06-26 10:41:47 -0700

    pin mop_util 2.1 version to use pyraisdk 0.3.7

--- packages/mop_utils/setup.py
+++ packages/mop_utils/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='mop_utils',
-    version='2.0',
+    version='2.1',
     description='Utility SDK for RAI Model Onboarding Pipeline (MOP) model contributor.',
     packages=['mop_utils'],
     package_dir={'mop_utils': './mop_utils'},
     python_requires='>=3.8.0',
     install_requires=[
-        'pyraisdk >= 0.2',
+        'pyraisdk ~= 0.3.7',
         'PyYAML >= 6.0',
         'numpy >= 1.22.0',
     ]
